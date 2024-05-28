9d5e8ff90ffd793285f19c22abcc11909c51530d
ReReenakub <namekikimaa@gmail.com>
2024-05-28 08:47:34 +0700

    Update ค้นหาตำแหน่ง

--- ตำแหน่ง.py
+++ ตำแหน่ง.py
@@ -8,6 +8,6 @@ position1 = pyautogui.position()
 print("ขยับเมาส์ไปที่ ชื่อลูกค้าในโปรแกรมคิว")
 time.sleep(5)
 position2 = pyautogui.position()
-
+#แสดงทั้งสองตำแหน่ง
 print(f"ตำแหน่งชื่อลูกค้า: {position1}")
 print(f"ตำแหน่งชื่อลูกค้าในโปรแกรมคิว: {position2}")
